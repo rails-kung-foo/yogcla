9c494573b9a1e64ce58fd239859a81eb05c467b2
Tayfun Topal <rails.kung.foo@gmail.com>
2014-08-19 13:26:56 +0200

    Admin validation stronger

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -4,7 +4,15 @@ class Admin < ActiveRecord::Base
   include Trimmer
  
   trimmed_fields :name, :admin_password, :admin_password_confirmation
-  validates :name, presence: true, uniqueness: true
+	
+	# name
+  validates :name, presence: true, uniqueness: true,length: { maximum: 50 }
+	
+	
+	# validates password
+	VALID_PW_REGX = /(?=.*[a-zA-Z])(?=.*[\d])(?=.*[\W]).{6,20}/
+	validates :password, format:{ with: VALID_PW_REGX },
+		 length: { minimum: 6, maximum: 20 }
   has_secure_password
  
  
