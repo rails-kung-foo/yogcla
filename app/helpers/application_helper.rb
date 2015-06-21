c1166f287c4680cca808e93dabe31edda162e86c
rails-kung-foo <rails.kung.foo@gmail.com>
2015-06-21 12:36:58 +0100

    Bugfix

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,7 @@ module ApplicationHelper
 	def is_active?(link_path)
 		current_page?(link_path) ? "active" : ""
 	end
-  
+
 		# Data for select field
 WEEKDAYS = [
 	"sunday",
@@ -21,28 +21,28 @@ GETSTYLES = Style.all.collect(&:stil)
 
 # add show all option
 def prepare_form(type)
-	if type = 'weekday'
+	if type == 'weekday'
 		prep_weekday = WEEKDAYS.clone
 		prep_weekday = prep_weekday.unshift('Select All')
 		return prep_weekday
 	end
-	
-	if type = 'studio'
+
+	if type == 'studio'
 		prep_studio = GETSTUDIOS.clone
 		prep_studio = prep_studio.unshift('Select All')
 		return prep_studio
 	end
-	
-	if type = 'style'
+
+	if type == 'style'
 		prep_style = GETSTYLES.clone
 		prep_style = prep_style.unshift('Select All')
 		return prep_style
 	end
-	
-	
+
+
 end
 
-	
-	
-	
+
+
+
 end
