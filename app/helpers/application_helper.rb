4eaf5ff073763b145050a591241dce1fc7c2de1b
rails-kung-foo <rails.kung.foo@gmail.com>
2015-06-21 16:54:14 +0100

    refactored #prepare_form

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,45 +4,26 @@ module ApplicationHelper
 		current_page?(link_path) ? "active" : ""
 	end
 
-		# Data for select field
-WEEKDAYS = [
-	"sunday",
-	"monday",
-	"tuesday",
-	"wednesday",
-	"thursday",
-	"friday",
-	"saturday"
-]
+	# Data for select field
+	WEEKDAYS = [
+		"sunday",
+		"monday",
+		"tuesday",
+		"wednesday",
+		"thursday",
+		"friday",
+		"saturday"
+	]
 
-# Set ready for select field. Get data from db!
-GETSTUDIOS = Studio.all.collect(&:name)
-GETSTYLES = Style.all.collect(&:stil)
+	# Set ready for select field. Get data from db!
+	GETSTUDIOS = Studio.all.collect(&:name)
+	GETSTYLES = Style.all.collect(&:stil)
 
-# add show all option
-def prepare_form(type)
-	if type == 'weekday'
-		prep_weekday = WEEKDAYS.clone
-		prep_weekday = prep_weekday.unshift('Select All')
-		return prep_weekday
+	# add show all option
+	def prepare_form(type)
+		options_list = Kernel.const_get("ApplicationHelper::#{type}")
+		options_list = options_list.unshift('Select All')
+		options_list
 	end
-
-	if type == 'studio'
-		prep_studio = GETSTUDIOS.clone
-		prep_studio = prep_studio.unshift('Select All')
-		return prep_studio
-	end
-
-	if type == 'style'
-		prep_style = GETSTYLES.clone
-		prep_style = prep_style.unshift('Select All')
-		return prep_style
-	end
-
-
-end
-
-
-
 
 end
