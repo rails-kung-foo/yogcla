14027185fa3fdbb6c514dfac0e09083e0dc2186b
rails-kung-foo <rails.kung.foo@gmail.com>
2015-06-22 16:36:11 +0100

    style test in progress

---
+++ spec/models/style_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe Style, type: :model do
+  it "has a valid factory"
+
+  it "is invalid with"
+
+  it "it downcases the stil"
+
+  it "trimms whitespaces from stil"
+  
+end
