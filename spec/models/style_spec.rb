d14ee03a9d2cd12590e0d6111f0cc34e6ca3ea71
rails-kung-foo <rails.kung.foo@gmail.com>
2015-06-23 12:05:06 +0100

    instance variable removed. Inserted let methods

--- spec/models/style_spec.rb
+++ spec/models/style_spec.rb
@@ -1,20 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe Style, type: :model do
-  it "downcases the stil" do
-    style_name = 'BIKRaM'
-    style = create(:style, stil: style_name)
-    expect(style.stil).to eq style_name.downcase
+  let(:valid_style){ create(:style) }
+
+  describe "before create" do
+    it "is downcases the stil" do
+      style_name = 'BIKRaM'
+      style = create(:style, stil: style_name)
+      expect(style.stil).to eq style_name.downcase
+    end
+
+    it "trimms whitespaces from stil" do
+      style = create(:style, stil: ' test ')
+      expect(style.stil).to eq 'test'
+    end
   end
 
-  it "trimms whitespaces from stil" do
-    style = create(:style, stil: ' test ')
-    expect(style.stil).to eq 'test'
-  end
-
-  describe "Validation" do
-    it "has a valid factory" do
-      expect(create(:style)).to be_valid
+  describe "ActiveModel validation" do
+   it "has a valid factory" do
+      expect(valid_style).to be_valid
     end
 
     it "is invalid with an empty field" do
@@ -23,27 +27,28 @@ RSpec.describe Style, type: :model do
     end
 
     it "is unique" do
-      first_style = create(:style)
+      valid_style
       second_style = build(:style)
       expect(second_style).to be_invalid
     end
   end
 
   describe "Methods" do
+    let(:second_record) { create(:style, stil: 'zen') }
     before :each do
-      @first = create(:style)
-      @second = create(:style, stil: 'zen')
+      valid_style
+      second_record
     end
 
     it "Style.stil_by returns matched record" do
       style = Style.by_stil('zen')
-      expect(style.first.id).to eq @second.id
+      expect(style.first.id).to eq second_record.id
       expect(style.count).to be 1
     end
 
     it "Style.latest returns the last updated_at record" do
       latest = Style.latest
-      expect(latest.id).to eq @second.id
+      expect(latest.id).to eq second_record.id
     end
   end
 end
