4f7cb2ebd5af1076b2ddc7c43f0a3307492814b2
rails-kung-foo <rails.kung.foo@gmail.com>
2015-06-23 16:27:30 +0100

    admin and session specs has prio

--- spec/controllers/styles_controller_spec.rb
+++ spec/controllers/styles_controller_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 RSpec.describe StylesController, type: :controller do
   let(:new_style){ create(:style) }
+  let(:session) {  }
 
   describe "GET #index" do
     before :each do
@@ -13,11 +14,48 @@ RSpec.describe StylesController, type: :controller do
     end
 
     it "populates array of stils" do
-      expect(assigns(:styles)).to eq [new_style]
+      new_style
+      expect(assigns(:styles).first.id).to eq new_style.id
+      expect(assigns(:styles).first.stil).to eq new_style.stil
     end
 
-    it "renders the index template" do
+    it "renders the :index template" do
       expect(response).to render_template :index
+    end
+  end
+
+  describe "GET #show" do
+    before :each do
+      new_style
+      get :show, id: new_style
+    end
+
+    it "is successful" do
+      expect(response.status).to eq 200
+    end
+
+    it "is renders the :show template" do
+      expect(response).to render_template :show
+    end
+
+    it "assigns the requested record @style" do
+      expect(assigns(:style)).to eq new_style
+    end
+  end
+
+  describe 'GET #new' do
+    before :each do
+      get :new
+    end
+
+    context "with no admin session" do
+      it "redirects to login_url" do
+        expect(response).to redirect_to login_url
+      end
+
+      it "flash notice is 'Please Log In'" do
+        expect(flash[:notice]).to eq "Please Log In"
+      end
     end
   end
 end
