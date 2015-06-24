c9801baa3e8c98b0e9e79a76d35404ebb09dd87b
rails-kung-foo <rails.kung.foo@gmail.com>
2015-06-24 21:46:57 +0100

    first steps of spec for course

---
+++ spec/factories/courses.rb
@@ -0,0 +1,10 @@
+FactoryGirl.define do
+  factory :course do |f|
+    studio_id 0
+    style_id 0
+    start '10:00'
+    #end '11:00'
+    weekday 'monday'
+  end
+
+end
---
+++ spec/models/course_spec.rb
@@ -0,0 +1,6 @@
+require 'rails_helper'
+
+RSpec.describe Course, type: :model do
+  let(:valid_course){ create(:course) }
+
+end
