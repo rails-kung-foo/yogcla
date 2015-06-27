e72243ea08a154d8a981d0cad983d6741f746f99
rails-kung-foo <rails.kung.foo@gmail.com>
2015-06-27 12:02:25 +0100

    specs for controller style in progress

--- spec/controllers/styles_controller_spec.rb
+++ spec/controllers/styles_controller_spec.rb
@@ -2,7 +2,8 @@ require 'rails_helper'
 
 RSpec.describe StylesController, type: :controller do
   let(:new_style){ create(:style) }
-  let(:session) {  }
+  let(:create_admin) { create(:admin) }
+  let(:session) { { admin_id: create_admin.id } }
 
   describe "GET #index" do
     before :each do
@@ -13,13 +14,12 @@ RSpec.describe StylesController, type: :controller do
       expect(response.status).to eq 200
     end
 
-    it "populates array of stils" do
+    it "populates array to @styles" do
       new_style
-      expect(assigns(:styles).first.id).to eq new_style.id
-      expect(assigns(:styles).first.stil).to eq new_style.stil
+      expect(assigns(:styles)).to match_array new_style
     end
 
-    it "renders the :index template" do
+    it "renders template :index" do
       expect(response).to render_template :index
     end
   end
@@ -34,28 +34,48 @@ RSpec.describe StylesController, type: :controller do
       expect(response.status).to eq 200
     end
 
-    it "is renders the :show template" do
+    it "renders template :show" do
       expect(response).to render_template :show
     end
 
-    it "assigns the requested record @style" do
+    it "assigns the requested record to @style" do
       expect(assigns(:style)).to eq new_style
     end
   end
 
   describe 'GET #new' do
-    before :each do
-      get :new
-    end
-
     context "with no admin session" do
+      before :each do
+        get :new
+      end
+
       it "redirects to login_url" do
         expect(response).to redirect_to login_url
       end
 
-      it "flash notice is 'Please Log In'" do
+      it "flashn notice is 'Please Log In'" do
         expect(flash[:notice]).to eq "Please Log In"
       end
     end
+
+    context "when admin is logged in" do
+      before :each do
+        create_admin
+        get :new, nil, session
+      end
+
+      it "is successful" do
+        expect(response).to have_http_status :success
+      end
+
+      it "renders template :new" do
+        expect(response).to render_template :new
+      end
+
+      it "assigns new style to @style" do
+        expect(assigns(:style)).to be_a_new Style
+      end
+    end
+
   end
 end
